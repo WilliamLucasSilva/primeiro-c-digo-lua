c02bc770b1637faa85721f754b3ce8f2ccad7435
William Lucas da Silva Ribeiro <llucas001500@gmail.com>
2024-02-12 11:22:48 -0300

    aprendendo orientação ao objeto em lua

--- main.lua
+++ main.lua
@@ -1,14 +1,25 @@
-nome = "robert"
-roberto = ""
+Pessoa = {
+    nome = '',
+    idade = '',
+    Detalhes = function(self)  -- Adicione 'self' como primeiro parâmetro para acessar os campos da tabela
+       love.graphics.print('Nome: ' .. self.nome .. ', ' .. 'Idade: ' .. self.idade, 400, 300)  -- Use 'self' para acessar os campos da tabela      
+    end
+}
+
+function Pessoa:nova(nome, idade)
+    novaPessoa = {}
+    setmetatable(novaPessoa, self)
+    self.__index = self
+    novaPessoa.nome = nome
+    novaPessoa.idade = idade
+    return novaPessoa
+end
 
 function love.load()
-    if nome == 'roberto' then
-        roberto = ' É o roberto'
-    else
-        roberto = ''
-    end
+    roberto = Pessoa:nova('roberto', 18)
 end
 
 function love.draw()
-    love.graphics.print(nome .. roberto, 400, 300) -- Usar `..` para concatenar strings em Lua
+    roberto:Detalhes()  -- Chame a função com ()
 end
+
